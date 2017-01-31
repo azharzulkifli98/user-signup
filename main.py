52805090a2f981fb580ad3bf7db9b89fef7120c9
azharzulkifli98 <azharzulkifli98@gmail.com>
2017-01-30 18:21:03 -0600

    finished setup but need to sanitize

--- main.py
+++ main.py
@@ -15,19 +15,54 @@
 # limitations under the License.
 #
 import webapp2
+import cgi
+
+page_header = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>User Signup</title>
+    <style type="text/css">
+        .error {
+            color: red;
+        }
+    </style>
+</head>
+<body>
+"""
+
+page_footer = """
+</body>
+</html>
+"""
+
+def page_content(user, pass1, pass2, email):
+    header = "<h2>SignUp</h2>"
+    username = "<td><label>username</td><td><input type='type' name='user' />" + user + "</label></td>"
+    password = "<td><label>password</td><td><input type='type' name='pass1'/>" + pass1 + "</label></td>"
+    confirm = "<td><label>confirm password</td><td><input type='type' name='pass2'/>" + pass2 + "</label></td>"
+    email = "<td><label>email (not needed)</td><td><input type='type' name='email'/>" + email + "</label></td>"
+    submit = "<input type='submit' />"
+    content = "<form method='post'>" + header + "<table border='0'><tr>" + username + "</tr><tr>" + password + "</tr><tr>"+ confirm + "</tr><tr>" + email + "</tr></table>" + submit + "</form>"
+    return content
+
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        header = """
-        <h2>SignUp</h2>
-        """
-        username = """<label>username<input type="type" /></label>"""
-        password = """<label>password<input type="type" /></label>"""
-        confirm = """<label>confirm password<input type="type" /></label>"""
-        email = """<label>email (not needed)<input type="type" /></label>"""
-        submit = """<input type="submit" />"""
-        content = "<form method='post'>" + header + username + "<br>" + password + "<br>" + confirm + "<br>" + email + "<br>" + submit + "</form>"
-        self.response.write(content)
+        content = page_content('', '', '', '')
+        clean = page_header + content + page_footer
+        self.response.write(clean)
+
+    def post(self):
+        user = self.request.get('user')
+        password = self.request.get('pass1')
+        confirm = self.request.get('pass2')
+        email = self.request.get('email')
+        page = page_content(user, pass1, pass2, email)
+        self.response.write("<h2>Welcome to the web, " + user + "</h2>")
+
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
